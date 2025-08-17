93ad2b359338b4e67ea9c2b41f33076334c02fa6
Disha Amin <debisha21@gmail.com>
2025-08-17 16:48:06 +0530

    all deleted

--- src/data_loader.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_resume_data(path):
-    df = pd.read_csv(path)
-    df.dropna(subset=['Resume_str', 'Category'], inplace=True)
-    return df
--- src/predict.py
+++
@@ -1,7 +0,0 @@
-import joblib
-
-def load_model(model_path='models/svm.pkl'):
-    return joblib.load(model_path)
-
-def predict(model, X):
-    return model.predict(X)
